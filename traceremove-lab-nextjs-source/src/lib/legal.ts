0a648b12a03f0a170dc939bc14602617b302986c
Artur Ziganshin <zikzk1394@gmail.com>
2026-08-11 18:00:49 +0200

    Add legal information constants to legal.ts

---
+++ traceremove-lab-nextjs-source/src/lib/legal.ts
@@ -0,0 +1,12 @@
+export const legalEntity = "Artur Ziganshin, trading as Traceremove";
+
+export const legalAddress =
+  "Registered address available on request and printed on every invoice";
+
+export const legalEmail = "support@traceremove.com";
+
+export const legalJurisdiction = "England and Wales";
+
+export const legalUpdated = "11 August 2026";
+
+export const merchantOfRecord = "Paddle.com Market Ltd";
